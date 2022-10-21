85d51e980603e0e3e34b45b6abdb78b26553b05d
Dominik.Peacock <der.dominik.peacock@gmail.com>
2022-10-21 13:18:45 +0200

    Added module for Vulkan support

---
+++ Source/TouchEngineVulkanRHI/Private/Exporting/TouchTextureExporterVulkan.h
@@ -0,0 +1,34 @@
+/* Shared Use License: This file is owned by Derivative Inc. (Derivative)
+* and can only be used, and/or modified for use, in conjunction with
+* Derivative's TouchDesigner software, and only if you are a licensee who has
+* accepted Derivative's TouchDesigner license or assignment agreement
+* (which also govern the use of this file). You may share or redistribute
+* a modified version of this file provided the following conditions are met:
+*
+* 1. The shared file or redistribution must retain the information set out
+* above and this list of conditions.
+* 2. Derivative's name (Derivative Inc.) or its trademarks may not be used
+* to endorse or promote products derived from this file without specific
+* prior written permission from Derivative.
+*/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Rendering/TouchTextureExporter.h"
+#include "TouchEngine/TouchObject.h"
+
+class UTexture2D;
+
+namespace UE::TouchEngine::Vulkan
+{
+	class FTouchTextureExporterVulkan : public FTouchTextureExporter
+	{
+	protected:
+
+		//~ Begin FTouchTextureExporter Interface
+		virtual FTouchExportResult ExportTexture_RenderThread(FRHICommandListImmediate& RHICmdList, const FTouchExportParameters& Params) override;
+		//~ End FTouchTextureExporter Interface
+	};
+}
+
