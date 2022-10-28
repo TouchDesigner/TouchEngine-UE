8c686141f4d2aa8d715b044d7ae635d044609344
Guy Lundvall <47373221+Drakynfly@users.noreply.github.com>
2022-10-27 19:33:42 -0700

    Add reimporting for tox asset

--- Source/TouchEngine/Private/ToxAsset.cpp
+++ Source/TouchEngine/Private/ToxAsset.cpp
@@ -1,3 +1,44 @@
 // Copyright © Derivative Inc. 2021
 
 #include "ToxAsset.h"
+
+#include "EditorFramework/AssetImportData.h"
+#include "UObject/ObjectSaveContext.h"
+
+void UToxAsset::PostInitProperties()
+{
+	UObject::PostInitProperties();
+
+#if WITH_EDITORONLY_DATA
+	if (!HasAnyFlags(RF_ClassDefaultObject))
+	{
+		AssetImportData = NewObject<UAssetImportData>(this, TEXT("AssetImportData"));
+	}
+#endif
+}
+
+void UToxAsset::PreSave(FObjectPreSaveContext SaveContext)
+{
+	UObject::PreSave(SaveContext);
+
+#if WITH_EDITORONLY_DATA
+	if (AssetImportData)
+	{
+		FString Path = AssetImportData->GetFirstFilename();
+		FPaths::MakePathRelativeTo(Path, *FPaths::ProjectContentDir());
+		FilePath = Path;
+	}
+#endif
+}
+
+#if WITH_EDITORONLY_DATA
+void UToxAsset::GetAssetRegistryTags(TArray<FAssetRegistryTag>& OutTags) const
+{
+	if (AssetImportData)
+	{
+		OutTags.Add(FAssetRegistryTag(SourceFileTagName(), AssetImportData->GetSourceData().ToJson(), FAssetRegistryTag::TT_Hidden));
+	}
+
+	Super::GetAssetRegistryTags(OutTags);
+}
+#endif
--- Source/TouchEngine/Public/ToxAsset.h
+++ Source/TouchEngine/Public/ToxAsset.h
@@ -16,7 +16,20 @@ class TOUCHENGINE_API UToxAsset : public UObject
 	GENERATED_BODY()
 
 public:
+	virtual void PostInitProperties() override;
+	virtual void PreSave(FObjectPreSaveContext SaveContext) override;
+
+#if WITH_EDITORONLY_DATA
+	virtual void GetAssetRegistryTags(TArray<FAssetRegistryTag>& OutTags) const override;
+#endif
+
 	// File path to the tox file, relative to the project directory
 	UPROPERTY(VisibleAnywhere, Category = ImportSettings)
 	FString FilePath;
+
+#if WITH_EDITORONLY_DATA
+	/** Import options used for this ToxAsset */
+	UPROPERTY(Category = ImportSettings, VisibleAnywhere, Instanced)
+	TObjectPtr<UAssetImportData> AssetImportData;
+#endif
 };
