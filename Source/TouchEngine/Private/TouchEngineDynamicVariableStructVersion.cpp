dc4f7db195184b5e5625464233236043e4c408ac
Voulz <12561988+Voulz@users.noreply.github.com>
2023-11-05 09:56:23 +1100

    Fix for Symbol Clash with RC Plugin

--- Source/TouchEngine/Private/TouchEngineDynamicVariableStructVersion.cpp
+++ Source/TouchEngine/Private/TouchEngineDynamicVariableStructVersion.cpp
@@ -19,4 +19,4 @@
 const FGuid FTouchEngineDynamicVariableStructVersion::GUID(0x42C318CE, 0xCE1911ED, 0xB79F8F5A, 0x0ED0B06D);
 
 // Register the custom version with core
-FCustomVersionRegistration GRegisterRemoteControlCustomVersion(FTouchEngineDynamicVariableStructVersion::GUID, FTouchEngineDynamicVariableStructVersion::LatestVersion, TEXT("TouchEngineDynamicVariableStructVer"));
+FCustomVersionRegistration GRegisterTouchEngineDynamicVariableStructCustomVersion(FTouchEngineDynamicVariableStructVersion::GUID, FTouchEngineDynamicVariableStructVersion::LatestVersion, TEXT("TouchEngineDynamicVariableStructVer"));
